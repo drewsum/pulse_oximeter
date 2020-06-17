a3c5892b4b87666c18192e85a07649b3df3b058f
Drew <drewmaatman2gmail.com>
2020-06-16 21:10:20 -0500

    started firmware project

---
+++ sfw/U1101_firmware/pulse_oximeter.X/main.c
@@ -0,0 +1,19 @@
+/*
+ * File:   main.c
+ * Author: Drew Maatman
+ *
+ * Created on June 16, 2020, 9:08 PM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    
+    
+    // endless loop
+    while(1) {
+        
+    }
+    
+}
